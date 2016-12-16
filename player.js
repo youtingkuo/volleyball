fddc5d6c71f2ce788f22e29cf9c990897a47b271
youtingkuo <kent0328111@gmail.com>
2016-12-16 10:21:46 +0800

    2.5 3

--- player.js
+++ player.js
@@ -376,7 +376,7 @@ player.prototype.makeTarget = function(){
 	
   //predictMesh.position.clone();
 	if(this.state === 4){
-    var x = Math.random() * 3.5 + 2;
+    var x = Math.random() * 2.5 + 3;
     var z = Math.random() * 6 - 3;
     this.preTarget.copy(predictMesh.position);
     if(this.isLeft){
